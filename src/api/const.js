d3a9853f8afab993b86648c4d132ee46be1c937a
no3utufff89 <14198908@mail.rugit config --global core.autocrlf truegit config --global core.safecrlf warngit config --global core.quotepath offgit add .git statusgit config --global user.email 14198908@mail.ru>
2023-09-01 00:55:20 +0300

    galary

--- src/api/const.js
+++ src/api/const.js
@@ -3,7 +3,7 @@ export const API_URL_AUTH = 'https://unsplash.com/oauth/authorize?';
 export const API_URL_TOKEN = 'https://unsplash.com/oauth/token?';
 export const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
 export const SECRET_KEY = process.env.REACT_APP_SECRET_KEY;
-export const REDIRECT_URI = 'http://192.168.1.42:3000';
+export const REDIRECT_URI = 'https://galary.onrender.com';
 export const RESPONSE_TYPE = 'code';
 export const SCOPE = 'public read_user read_photos write_likes';
 
